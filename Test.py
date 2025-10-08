aac69bf9657d1e18a5c0a95ec3d3e3c23afdcaac
MrSirDude <104992232+ski-woozy@users.noreply.github.com>
2025-10-08 12:37:41 +0700

    New project and comment updates

--- Test.py
+++ Test.py
@@ -20,7 +20,7 @@ x = int(x)
 if x == 1:
     
     #Project Title
-    print("Greet Project")
+    print("Project Title: Greet Project")
     
     userName = str()
     userBday = str()
@@ -34,10 +34,15 @@ if x == 1:
 elif x == 2:
     
     #Project Title
-    print("Number Guessing Project")
+    print("Project Title: Number Guessing Project")
+
+    # 1. Input a number between 1 - 100
+    # 2. Check if it's lower or higher than 50
+    # 3. Check if it's higher or lower than 25/75
+    # 4. Increase or decrease count check while printing the iterations
     
     countCheck = int(50)
-    print("Ill guess your number, try me: ")
+    print("Ill guess your number, give me a number between 1 - 100: ")
     numberIn = input()
     numberIn = int(numberIn)
     
@@ -88,32 +93,45 @@ elif x == 2:
 #Project Select #3 
 elif x == 3:
     
+    # 1. Receive a random set of number
+    # 2. Transfer into an array
+    # 3. Set variables for highest and lowest numbers, initial value is set to none
+    # 4. Value of the first item in the array will be set as the highest or lowest value at the start of iteration.
+    # 5. Variables will be updated if the array item is smaller or bigger than current values
+    # 6. Print highest and lowest numbers.
+    
     #Project Title
-    print("BIGGEST and LOWEST number project")
+    print("Project Title: HIGHEST and LOWEST number project")
     
     numSet = []
     giveRandomSet(numSet)
     print(numSet)
     
     lowestNumber = None
-    biggestNumber = None
+    highestNumber = None
     
     for number in numSet:
-        if biggestNumber is None or number > biggestNumber:
-            biggestNumber = number
+        if highestNumber is None or number > highestNumber:
+            highestNumber = number
     
     for number in numSet:
         if lowestNumber is None or number < lowestNumber:
             lowestNumber = number
 
     print("the lowest number is ",lowestNumber)
-    print("the biggest number is ",biggestNumber)
+    print("the highest number is ",highestNumber)
     
 #Project Select #4    
 elif x == 4:
     
+    # 1. Receive a word
+    # 2. Find the length of the word (how many characters total)
+    # 3. Set a variable to represent index number
+    # 4. Print each letter accompanied by their index placement
+    # 5. Print word length
+    
     #Project Title
-    print("String Character Counter and Index Coded Character Print")
+    print("Project Title: String Character Counter and Index Coded Character Print")
     
     wordInput = input('Type a word: ')
     wordLen = len(wordInput)  
@@ -123,6 +141,43 @@ elif x == 4:
         indNum = indNum + 1
     print("Word length is ", wordLen," characters")
     
-#Project Selection Failure
+#Project Select #5
+elif x == 5:
+    
+    # 1. Receive a sentence
+    # 2. Break into words (ignore numbers or special characters) 
+    # 3. Place into array
+    # 4. Check length of each word
+    # 5. Print longest word
+    
+    #Project Title
+    print("Project Title: Finding the longest word in the sentence")
+    
+    #Input Sentence and Variables Declaration
+    inputSentence = input("Type a sentence\n")
+    wordList = []
+    wordList = inputSentence.split()
+    wordListClean = []
+    longestWord = ''
+    
+    #Cleans wordList of non-alpha characters
+    for word in wordList:
+        cleanWord = ''
+        if word.isalpha():
+            wordListClean.append(word)
+        elif not word.isalpha():
+            for letter in word:
+                if letter.isalpha():
+                    cleanWord = cleanWord + letter
+            wordListClean.append(cleanWord)
+            
+    #Finds longest word and prints all words with their character count
+    for word in wordListClean:
+        if len(word) > len(longestWord):
+            longestWord = word
+        print(word, " - ", len(word), " characters")
+    print("The longest word is ", longestWord, " with ", len(longestWord), " characters")
+    
+#Project Select Fail
 else:
-    print("hatdog")
+    print("Project not found, please try again.")
