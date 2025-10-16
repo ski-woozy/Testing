0fb9684895f4e66a3c437aba939aecdde1bc0c18
MrSirDude <104992232+ski-woozy@users.noreply.github.com>
2025-10-16 12:39:56 +0700

    Update Basic Greet.py
    Fixed the error with the structure

--- Basic Greet.py
+++ Basic Greet.py
@@ -1,15 +1,15 @@
 #Project Title
-    print("\nProject Title: Basic Greet")
-    
-    # Functions for the Project
-    def greet(usrName, birthDate):
-        return print("\nYour name is " + usrName + ' and you were born on ' + birthDate + '. ' + 'Nice to meet you!\n')
+print("\nProject Title: Basic Greet")
 
-    #User-Program Interaction
-    userName = str()
-    userBday = str()
+# Functions for the Project
+def greet(usrName, birthDate):
+    return print("\nYour name is " + usrName + ' and you were born on ' + birthDate + '. ' + 'Nice to meet you!\n')
 
-    userName = input('\nWhat is your name?\n')
-    userBday = input('\nWhen were you born?\n')
-    
-    greet(userName, userBday)
+#User-Program Interaction
+userName = str()
+userBday = str()
+
+userName = input('\nWhat is your name?\n')
+userBday = input('\nWhen were you born?\n')
+
+greet(userName, userBday)
